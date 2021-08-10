5afb93eab825643e7ef3711982785ad8ae44ea85
dg12988 <doug.goldberg@me.com>
2021-08-10 03:09:02 -0400

    1 file modified

--- index.ts
+++ index.ts
@@ -1,2 +1,20 @@
+import { forkJoin, Observable } from "rxjs";
 
 
+const a$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.next('A');
+    subscriber.complete();
+  }, 3000);
+});
+
+const b$ = new Observable(subscriber => {
+  setTimeout(() => {
+    subscriber.error('Failure');
+  }, 5000);
+});
+
+forkJoin([a$, b$]).subscribe({
+  next: value => console.log(value),
+  error: err => console.log('Error', err)
+});
